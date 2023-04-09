a294b2aa7d0de6d4d63f512b8ef3a5e609b42947
Raiber <raibercuba@gmail.com>
2023-04-09 19:56:36 -0300

    Primer commit

---
+++ bin/raiber_proyecto_inicial.dart
@@ -0,0 +1,5 @@
+import 'package:raiber_proyecto_inicial/raiber_proyecto_inicial.dart' as raiber_proyecto_inicial;
+
+void main(List<String> arguments) {
+  print('Hello world: ${raiber_proyecto_inicial.calculate()}!');
+}
---
+++ test/raiber_proyecto_inicial_test.dart
@@ -0,0 +1,8 @@
+import 'package:raiber_proyecto_inicial/raiber_proyecto_inicial.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
